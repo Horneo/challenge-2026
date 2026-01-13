0c74d84563fe78dbec24a70765125fcf85245bae
Horneo <niccolas12@hotmail.com>
2026-01-13 12:52:13 -0300

    feat: nuevo proyecto acreditaciones

---
+++ nuevo ms acreditaciones/acreditaciones/src/main/java/com/example/acreditaciones/dto/FundAccreditation.java
@@ -0,0 +1,12 @@
+package com.example.acreditaciones.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+
+public record FundAccreditation(
+        @NotBlank(message = "Debe ingresar un monto obligatoriamente y no puede estar vacío.")
+        BigDecimal amount,
+        @NotBlank(message = "Debe ingresar un id obligatoriamente y no puede estar vacío.")
+        String idPointOfSale) {
+}
---
+++ nuevo ms acreditaciones/acreditaciones/src/test/java/com/example/acreditaciones/AcreditacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acreditaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcreditacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
